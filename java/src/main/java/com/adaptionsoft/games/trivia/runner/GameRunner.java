2c60a0c1d76b8638bc135d66c2bf48beb68979b3
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2023-03-31 15:24:06 -0700

    Rename Game class to TriviaGame

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,36 +1,34 @@
 
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
-import com.adaptionsoft.games.uglytrivia.Game;
-
+import com.adaptionsoft.games.uglytrivia.TriviaGame;
 
 public class GameRunner {
 
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
+		TriviaGame triviaGame = new TriviaGame();
+
+		triviaGame.add("Chet");
+		triviaGame.add("Pat");
+		triviaGame.add("Sue");
+
 		Random rand = new Random();
-	
+
 		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
+
+			triviaGame.roll(rand.nextInt(5) + 1);
+
 			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
+				notAWinner = triviaGame.wrongAnswer();
 			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
+				notAWinner = triviaGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
 		} while (notAWinner);
-		
+
 	}
 }
